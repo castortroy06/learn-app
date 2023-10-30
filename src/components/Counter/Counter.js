d8369f4c901aa299d8239869aacfd69655e2945c
Denis Pelyukhow <denis_pelyukhow@epam.com>
2023-10-30 14:13:31 +0300

    Lesson 1: tasks 1,2,3
    # Conflicts:
    #	src/App.js
    #	src/components/Counter/Counter.js
    #	src/components/GenreSelect.js
    #	src/components/SearchForm/SearchForm.js

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import '../../sass/components/button-holder.scss';
 
 class Counter extends React.Component {
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import '../../sass/components/search-form.scss';
 
 
