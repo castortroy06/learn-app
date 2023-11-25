d01294bbb559fee15c8f21d15101d7de4cc6f8bc
Denis Pelyukhow <denis_pelyukhow@epam.com>
2023-11-25 19:39:56 +0400

    Lesson 6: Routes

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovie } from '../../api/MovieAPI';
 import './movie-details.scss';
 
-const MovieDetails = (props) => {
+const MovieDetails = () => {
+    const { movieId } = useParams();
+    const [props, setProps] = useState({});
+
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+                const response = await fetchMovie(movieId);
+                setProps(response);
+
+            } catch (e) {
+
+            }
+        };
+
+        loadData();
+    }, [movieId]);
 
     return (
         <div className="movie-details row">
             <img className="col-4" data-testid="image" alt={props.title} src={props.poster_path} />
             <div className="movie-details--container col-8">
                 <h2 data-testid="name" className="movie-details--name">{props.title}<span className="movie-details--rate">{props.vote_average}</span></h2>
-                <p data-testid="categories" className="movie-details--categories">{props.genres.join(', ')}</p>
+                <p data-testid="categories" className="movie-details--categories">{props['genres'] !== undefined ? props.genres.join(', ') : ''}</p>
                 <p className="movie-details--year-duration">
                     <span data-testid="year" className="movie-details--year">{props.release_date}</span>
                     <span data-testid="duration" className="movie-details--duration">{props.runtime}</span>
@@ -18,9 +36,5 @@ const MovieDetails = (props) => {
         </div>
     );
 };
-
-MovieDetails.defaultProps = {
-    genres: ['Adventures', 'Comedy'],
-}
 
 export default MovieDetails;
